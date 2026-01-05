92f1cd07f2175eefc0afa4d127f857946bb775e2
Kelvin <kelvinuneze@gmail.com>
2026-01-05 17:24:47 -0500

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // <-- make sure this matches deployment
-  plugins: [react()]
+  plugins: [react()],
+  base: "/starlife-movies/", // 👈 MUST MATCH REPO NAME
 });
-
